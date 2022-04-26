8c27680f6f7a192f5b72f9ca8ab80b1a8d3df721
Ardian-Kurniawan <56443318+Ardiankls@users.noreply.github.com>
2022-04-26 16:22:02 +0700

    Initial Commit

---
+++ bepcalculator/bepcalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  bepcalculatorApp.swift
+//  bepcalculator
+//
+//  Created by ardian kurniawan on 26/04/22.
+//
+
+import SwiftUI
+
+@main
+struct bepcalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
